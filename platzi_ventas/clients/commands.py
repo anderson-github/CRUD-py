cb33021d93ae1d04d2a70fe7716d58036907667e
Anderson RS <ander.irs@gmail.com>
2020-03-02 07:02:02 -0300

    First commit

---
+++ platzi_ventas/clients/commands.py
@@ -0,0 +1,57 @@
+import click
+
+from clients.services import ClientService
+from clients.models import Client
+
+
+@click.group()
+def clients():
+	"""Manages clients lifecycle"""
+	pass
+
+
+@clients.command()
+@click.option('-n', '--name', type=str,
+				prompt=True, 
+				help='The client name')
+@click.option('-c', '--company', type=str,
+				prompt=True, 
+				help='The client company')
+@click.option('-e', '--email', type=str,
+				prompt=True, 
+				help='The client email')
+@click.option('-p', '--position', type=str,
+				prompt=True, 
+				help='The client position')
+@click.pass_context
+def create(ctx, name, company, email, position):
+	"""Create a new client"""
+	client = Client(name, company, email, position)
+	client_service = ClientService(ctx.obj['clients_table'])
+	
+	client_service.create_client(client)
+
+
+@clients.command()
+@click.pass_context
+def list(ctx):
+	"""List all clients"""
+	pass
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+	"""Updates a client info"""
+	pass
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_uid):
+	"""Deletes a client"""
+	pass
+
+
+all = clients
+
